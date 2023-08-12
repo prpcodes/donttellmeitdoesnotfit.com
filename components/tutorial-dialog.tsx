58c6a98d7e163e3222643862f247b4761228aa69
Philipp R. Proksch <mail@prp.wien>
2023-08-12 16:04:23 +0200

    refactored the styling

--- components/tutorial-dialog.tsx
+++ components/tutorial-dialog.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
 import {
   Dialog,
@@ -8,8 +10,12 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
+import { Input } from "./ui/input";
+import { useState } from "react";
+import { Label } from "./ui/label";
 
 export function TutorialDialog() {
+  const [width, setWith] = useState(0);
   return (
     <Dialog>
       <DialogTrigger asChild>
@@ -23,29 +29,37 @@ export function TutorialDialog() {
       <DialogContent className="sm:max-w-[425px]">
         <DialogHeader>
           <DialogTitle>How To Measure Your Penis</DialogTitle>
-          <DialogDescription>
-            <p className="my-2">
-              You need a soft measuring tape or a piece of string and a ruler to
-              measure the string.
-            </p>
-            <ol className="my-6 ml-6 list-decimal [&>li]:mt-2">
-              <li>
-                Place the measuring tape or string at the base of your{" "}
-                <b>ERECT</b> penis and measure from the bottom to the top, make
-                sure your penis is <b>central</b> and <b>straight</b>.
-              </li>
-              <li>
-                Measure all the way around your <b>ERECT</b> penis at the
-                mid-point of the shaft, or measure the circumference at the head
-                (choose the widest part).
-              </li>
-              <li>
-                Divide your penis circumference by <b>2.25</b> to make up for
-                the about 10% to 20% stretch.
-              </li>
-            </ol>
-          </DialogDescription>
+          <p className="text-sm text-muted-foreground">
+            You need a soft measuring tape or a piece of string and a ruler to
+            measure the string.
+          </p>
+          <ol className="text-sm text-muted-foreground ml-6 list-decimal [&>li]:mt-2 text-left">
+            <li>
+              Place the measuring tape or string at the base of your{" "}
+              <b>ERECT</b> penis and measure from the bottom to the top, make
+              sure your penis is <b>central</b> and <b>straight</b>.
+            </li>
+            <li>
+              Measure all the way around your <b>ERECT</b> penis at the
+              mid-point of the shaft, or measure the circumference at the head
+              (choose the widest part).
+            </li>
+            <li>
+              Divide your penis circumference by <b>2.25</b> to make up for the
+              about 10% to 20% stretch.
+            </li>
+          </ol>
         </DialogHeader>
+        <DialogFooter className="flex-row items-center !justify-center text-sm text-muted-foreground gap-2">
+          <Label htmlFor="width">Width:</Label>
+          <Input
+            className="w-24"
+            type="number"
+            placeholder="130mm"
+            onChange={(e) => setWith(Math.round(Number(e.target.value) / 2.25))}
+          />
+          <span>÷ 2.25 = {width}mm</span>
+        </DialogFooter>
       </DialogContent>
     </Dialog>
   );
