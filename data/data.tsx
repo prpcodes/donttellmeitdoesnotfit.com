729acd8bdb47e79a842abab8dcbba6a81ddce5c0
Philipp R. Proksch <mail@prp.wien>
2023-08-13 15:03:45 +0200

    removed unused types

--- data/data.tsx
+++ data/data.tsx
@@ -1,104 +1,30 @@
-import {
-  ArrowDownIcon,
-  ArrowRightIcon,
-  ArrowUpIcon,
-  CheckCircledIcon,
-  CircleIcon,
-  CrossCircledIcon,
-  QuestionMarkCircledIcon,
-  StopwatchIcon,
-  ValueNoneIcon,
-} from "@radix-ui/react-icons";
-
-export const labels = [
-  {
-    value: "bug",
-    label: "Bug",
-  },
-  {
-    value: "feature",
-    label: "Feature",
-  },
-  {
-    value: "documentation",
-    label: "Documentation",
-  },
-];
-
-export const statuses = [
-  {
-    value: "backlog",
-    label: "Backlog",
-    icon: QuestionMarkCircledIcon,
-  },
-  {
-    value: "todo",
-    label: "Todo",
-    icon: CircleIcon,
-  },
-  {
-    value: "in progress",
-    label: "In Progress",
-    icon: StopwatchIcon,
-  },
-  {
-    value: "done",
-    label: "Done",
-    icon: CheckCircledIcon,
-  },
-  {
-    value: "canceled",
-    label: "Canceled",
-    icon: CrossCircledIcon,
-  },
-];
-
 export const materials = [
-  {
-    value: "nitrileRubber",
-    label: "Nitrile Rubber",
-  },
   {
     value: "polyisoprene",
     label: "Polyisoprene",
   },
   {
-    value: "polyurethane",
-    label: "Polyurethane",
+    value: "vytex",
+    label: "Vytex",
   },
   {
-    value: "latex",
-    label: "Latex",
-  },
-  {
-    value: "veganLatex",
-    label: "Vegan Latex",
-  },
-  {
-    value: "sheerlonLatex",
-    label: "Sheerlon Latex",
-  },
-  {
-    value: "unclassified",
-    label: "Unclassified",
+    value: "natural latex",
+    label: "Natural Latex",
   },
 ];
 
-export const priorities = [
+export const lubricants = [
   {
-    label: "Low",
-    value: "low",
-    icon: ArrowDownIcon,
+    value: "silicone",
+    label: "Silicone",
   },
   {
-    label: "Medium",
-    value: "medium",
-    icon: ArrowRightIcon,
+    value: "water-based",
+    label: "Water-Based",
   },
   {
-    label: "High",
-    value: "high",
-    icon: ArrowUpIcon,
+    value: "dimeticon",
+    label: "Dimeticon",
   },
 ];
 
@@ -113,7 +39,7 @@ export const types = [
   },
 ];
 
-export const isVegan = [
+export const isvegan = [
   {
     label: "Yes",
     value: "true",
