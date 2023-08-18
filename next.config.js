364626f6adc916cd84dc037dfaf12aacad9e2937
Philipp R. Proksch <mail@prp.wien>
2023-08-18 09:42:57 +0200

    added CORS-Headers

--- next.config.js
+++ next.config.js
@@ -1,4 +1,28 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: [
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "https://ackee-dj6u.onrender.com",
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,OPTIONS,PATCH,POST",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value: "Authorization, Content-Type, Time-Zone",
+          },
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Max-Age", value: "3600" },
+        ],
+      },
+    ];
+  },
+};
 
 module.exports = nextConfig;
