5b86d644fe5b769bc28302e7859b2b0a4c4c4830
Philipp R. Proksch <mail@prp.wien>
2023-08-18 11:19:43 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,28 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  async headers() {
-    return [
-      {
-        source: "/:path*",
-        headers: [
-          {
-            key: "Access-Control-Allow-Origin",
-            value: "https://ackee-dj6u.onrender.com",
-          },
-          {
-            key: "Access-Control-Allow-Methods",
-            value: "GET,OPTIONS,PATCH,POST",
-          },
-          {
-            key: "Access-Control-Allow-Headers",
-            value: "Authorization, Content-Type, Time-Zone",
-          },
-          { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Max-Age", value: "3600" },
-        ],
-      },
-    ];
-  },
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
